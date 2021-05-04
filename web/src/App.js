2f025750f8914b44438083641cb0c89e8da82d8a
SkyHawkDP <david@prec-design.com>
2021-05-04 10:43:43 -0400

    Create Base App

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,10 @@
 import { FatalErrorBoundary } from '@redwoodjs/web'
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 
+import { Router, Route } from '@redwoodjs/router'
 import FatalErrorPage from 'src/pages/FatalErrorPage'
 import Routes from 'src/Routes'
-
+// Bare Base App for Redwood
 import './index.css'
 
 const App = () => (
